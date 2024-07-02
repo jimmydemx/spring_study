97cd37ff5ef9aa6b5b8a2eed1294cb77618aa10a
jamesdemx <j_demx@yahoo.com>
2024-07-02 08:31:06 +0200

    add jwt

--- sky-server/src/main/java/com/study/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/study/handler/GlobalExceptionHandler.java
@@ -1,7 +1,8 @@
 package com.study.handler;
-
+import com.study.exception.BaseException;
 import com.study.constant.MessageConstant;
 import com.study.result.Result;
+import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,6 +14,12 @@ import java.sql.SQLIntegrityConstraintViolationException;
 public class GlobalExceptionHandler {
 
 
+
+    @ExceptionHandler
+    public Result exceptionHandler(BaseException ex){
+        return Result.error(ex.getMessage());
+
+    }
     /**
      *  SQL Exception handler
      * @params
@@ -20,7 +27,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler
     public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
         String message = ex.getMessage();
-        if(message.contains("Dulicate entry")){
+        if(message.contains("Duplicate entry")){
             String[] split = message.split(" ");
             String username = split[2];
             String msg = username+ MessageConstant.ALREADY_EXISTS;
